bd78c3aab449944af1e258ebf039f4f6e8d795d4
dreamthinkstudios <emily.c.chen@gmail.com>
2016-11-01 17:03:41 -0400

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 <h1>SPY QUIZ</h1>
 
-Do you have what it takes to be a spy?
+WORK IN PROGRESS...
+
+<strong><a href="https://dreamthink.github.io/spyquiz" target="_blank">View demo</a></strong>
+
+Do you have what it takes to be a spy? Find out with this Spy Quiz.
+
+<h3>BUILT WITH:</h3>
+HTML, CSS, JavaScript, jQuery
+
+<strong><a href="https://dreamthink.github.io/spyquiz" target="_blank">View demo</a></strong>
--- js/app.js
+++ js/app.js
@@ -1,7 +1,60 @@
+"use strict";
+
 $(document).ready(function() {
 	$("html").on("click", function() {
 		$(".spyimage").fadeIn(3000);
 		$(".btn-start").fadeIn(5000);
 	});
 
+	$(".btn-start").on("click", function() {
+		$(".intro").hide();
+		$("main").show();
+		$(".questionText").html(questionsArray[0].question);
+		$(".possibleAnswers1").html(questionsArray[0].choices);
+	});
+
 });
+
+var currentQuestionNumber = 0,
+correctTotal = 0,
+totalNumberOfQuestions = questionsArray.length;
+
+function questionDisplay() {
+	$("#prompt").text('"' + questionsArray[currentQuestionNumber].questionText + '"');
+	$("#choices").empty();
+	var totalNumberOfChoices = questionsArray[currentQuestionNumber].questionChoices.length;
+	for (var i = 0; i < totalNumberOfChoices; i++) {
+		var buildEachChoiceHTML
+	}
+
+}
+
+var questionsArray = [
+	{
+		questionText: "How do you feel about learning foreign languages?",
+		questionChoices: ["Hola! 你好! Bonjour! Привет!", "I speak one foreign language", "I know a few words", "One language is enough of a challenge"], 
+		correctAnswer: 0
+	},
+	{
+		questionText: "You think you are being followed. What do you do?",
+		questionChoices: ["Confront the person directly and ask if they are following you", "Run as fast as you can!", "Change your route immediately", "Let the person follow you and discreetly try to get a detailed physical description"],
+		correctAnswer: 3
+	},
+	{
+		questionText: "When at a party, you...",
+		questionChoices: ["Attend parties strictly for the food", "Introduce yourself to everyone", "Stand in a corner and wait for someone to approach you", "Talk only to people you know"],
+		correctAnswer: 1
+	},
+	{
+		questionText: "Can you protect yourself in dangerous situations?",
+		questionChoices: ["I'm a ninja", "Yikes! Help me!", "I can throw a punch or two", "I'm a scaredy cat"],
+		correctAnswer: 0
+	},
+	{
+		questionText: "Can you keep a secret?",
+		questionChoices: ["Absolutely not", "Maybe, depends on what it is", "My lips are sealed", "Can I tell one or two people?"],
+		correctAnswer: 2
+	}
+];
+
+
