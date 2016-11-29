497b46f124c4e2f363708634e3680f6729ef9f68
dreamthinkstudios <emily.c.chen@gmail.com>
2016-11-28 22:27:41 -0600

    Working quiz functionality

--- js/app.js
+++ js/app.js
@@ -1,60 +1,99 @@
 "use strict";
 
-$(document).ready(function() {
-	$("html").on("click", function() {
-		$(".spyimage").fadeIn(3000);
-		$(".btn-start").fadeIn(5000);
-	});
-
-	$(".btn-start").on("click", function() {
-		$(".intro").hide();
-		$("main").show();
-		$(".questionText").html(questionsArray[0].question);
-		$(".possibleAnswers1").html(questionsArray[0].choices);
-	});
-
-});
-
-var currentQuestionNumber = 0,
-correctTotal = 0,
-totalNumberOfQuestions = questionsArray.length;
-
-function questionDisplay() {
-	$("#prompt").text('"' + questionsArray[currentQuestionNumber].questionText + '"');
-	$("#choices").empty();
-	var totalNumberOfChoices = questionsArray[currentQuestionNumber].questionChoices.length;
-	for (var i = 0; i < totalNumberOfChoices; i++) {
-		var buildEachChoiceHTML
-	}
-
-}
-
 var questionsArray = [
 	{
 		questionText: "How do you feel about learning foreign languages?",
 		questionChoices: ["Hola! 你好! Bonjour! Привет!", "I speak one foreign language", "I know a few words", "One language is enough of a challenge"], 
-		correctAnswer: 0
+		correctAnswer: 0,
+		correctDetails: "Hola! 你好! Bonjour! Привет!"
 	},
 	{
 		questionText: "You think you are being followed. What do you do?",
 		questionChoices: ["Confront the person directly and ask if they are following you", "Run as fast as you can!", "Change your route immediately", "Let the person follow you and discreetly try to get a detailed physical description"],
-		correctAnswer: 3
+		correctAnswer: 3,
+		correctDetails: "Let the person follow you and discreetly try to get a detailed physical description"
 	},
 	{
 		questionText: "When at a party, you...",
 		questionChoices: ["Attend parties strictly for the food", "Introduce yourself to everyone", "Stand in a corner and wait for someone to approach you", "Talk only to people you know"],
-		correctAnswer: 1
+		correctAnswer: 1,
+		correctDetails: "Introduce yourself to everyone"
 	},
 	{
 		questionText: "Can you protect yourself in dangerous situations?",
 		questionChoices: ["I'm a ninja", "Yikes! Help me!", "I can throw a punch or two", "I'm a scaredy cat"],
-		correctAnswer: 0
+		correctAnswer: 0,
+		correctDetails: "I'm a ninja"
 	},
 	{
 		questionText: "Can you keep a secret?",
 		questionChoices: ["Absolutely not", "Maybe, depends on what it is", "My lips are sealed", "Can I tell one or two people?"],
-		correctAnswer: 2
+		correctAnswer: 2,
+		correctDetails: "My lips are sealed"
 	}
 ];
 
+var currentQuestionNumber = 0,
+correctTotal = 0,
+totalNumberOfQuestions = questionsArray.length,
+userSelectedChoiceArray = [];
+
+function questionDisplay() {
+	$("#prompt").text(questionsArray[currentQuestionNumber].questionText);
+	$("#choices").empty();
+	var totalNumberOfChoices = questionsArray[currentQuestionNumber].questionChoices.length;
+	for (var i = 0; i < totalNumberOfChoices; i++) {
+		var buildEachChoiceHTML = "<input type='radio' class='option' value=" + i + "> " + questionsArray[currentQuestionNumber].questionChoices[i] + "<br>";
+		$("#choices").append(buildEachChoiceHTML);
+		// userSelectedChoiceArray.push()
+	}
+	$(".questionNumberDisplay").text("Question " + (currentQuestionNumber + 1) + " of " + totalNumberOfQuestions);
+}
+
+$(document).ready(function() {
+	$(".css-typing").show();
+	$("html").on("click", function() {
+			$(".spyimage").fadeIn(3000);
+			$(".btn-start").fadeIn(5000);
+		});
+	$(".btn-start").on("click", function() {
+		$(".intro").hide();
+		$("main").show();
+		$(".results").hide();
+		questionDisplay();
+	});
+
+	$(".questionAndAnswerContainer").on("click", ".option", function() {
+		var answer = $("input[class='option']:checked").val();
+		userSelectedChoiceArray.push(answer);
+		var correctAnswer = questionsArray[currentQuestionNumber].correctAnswer;
+		if (answer == correctAnswer) {
+			correctTotal++;
+			console.log(correctTotal);
+		}
+	$("#result-msg").append("<h3>Q: " + questionsArray[currentQuestionNumber].questionText + "</h3>");
+	$("#result-msg").append("<h4>A: " + questionsArray[currentQuestionNumber].correctDetails + "</h4>");
+
+	if (currentQuestionNumber + 1 == totalNumberOfQuestions) {
+		$("#result").text(correctTotal + "/" + totalNumberOfQuestions);
+		$(".questionAndAnswerContainer").hide();
+		$(".results").show();
+	} else {
+		currentQuestionNumber++;
+		questionDisplay();
+	}
+	});
+
+	$("#againButton").on("click", function() {
+		currentQuestionNumber = 0;
+		correctTotal = 0;
+		$("#result-msg").empty();
+		$(".results").hide();
+		$(".questionAndAnswerContainer").show();
+		questionDisplay();
+	});
+
+// console.log(userSelectedChoiceArray);
+
+});
 
